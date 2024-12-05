5e5dbca0ebaa946daa79e1169d61a4d312135135
Tolevats <pstavelot@gmail.com>
2024-12-05 01:02:42 -0300

    update: Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    <p>&copy; 2024 Tolevats. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'component-footer',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+.footer {
+  background-color: #1f1f1f;
+  color: white;
+  text-align: center;
+  padding: 1em 0;
+  margin-top: 2em;
+}
+</style>
